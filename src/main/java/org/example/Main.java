2f3a1ec71b49dea5c861d804162f7172ca896e66
Stefan <167103623+DeFicker27@users.noreply.github.com>
2024-09-16 09:46:26 +0200

    Klassenerstellung

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import java.awt.print.Book;
+import java.io.Console;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var book = new StoryReader.read();
+        var console = new Console();
+        var narrator = new StoryTeller(book, console);
+        narrator.onceUponATime
     }
 }
